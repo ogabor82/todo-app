dbbbeacde2058adb4e38777d798a83fac688d202
Ocskó Gábor <ogabor@gmail.com>
2024-12-04 10:08:45 +0100

    Added Tailwind CSS

---
+++ frontend/src/components/TodoContainer/TodoContainer.tsx
@@ -0,0 +1,9 @@
+const TodoContainer = () => {
+  return (
+    <div className="flex flex-col gap-4 border-2 border-gray-500 rounded-md p-4">
+      <h1>Todo List</h1>
+    </div>
+  );
+};
+
+export default TodoContainer;
